7494a33e946ff5b1a24b70cd1af0bc8b17533749
DerZyklop <mail@der-zyklop.de>
2014-06-24 20:17:33 +0200

    lot of template changes

--- index.php
+++ index.php
@@ -355,24 +355,20 @@ $dirs = @array_values($dirs); $files = @array_values($files);
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Directory Listing of <?php echo str_replace('\\', '', dirname(strip_tags($_SERVER['PHP_SELF']))).'/'.$leadon;?></title>
 <link rel="stylesheet" type="text/css" href="<?php echo $includeurl; ?>build/css/styles.css" />
-<?php
-if($showthumbnails) {
-?>
-<script language="javascript" type="text/javascript">
-<!--
-function o(n, i) {
-	document.images['thumb'+n].src = '<?php echo $includeurl; ?>dlf/i.php?f='+i<?php if($memorylimit!==false) echo "+'&ml=".$memorylimit."'"; ?>;
+<?php if($showthumbnails) : ?>
+	<script language="javascript" type="text/javascript">
+	<!--
+	function o(n, i) {
+		document.images['thumb'+n].src = '<?php echo $includeurl; ?>dlf/i.php?f='+i<?php if($memorylimit!==false) echo "+'&ml=".$memorylimit."'"; ?>;
 
-}
+	}
 
-function f(n) {
-	document.images['thumb'+n].src = 'dlf/trans.gif';
-}
-//-->
-</script>
-<?php
-}
-?>
+	function f(n) {
+		document.images['thumb'+n].src = 'dlf/trans.gif';
+	}
+	//-->
+	</script>
+<?php endif; ?>
 </head>
 <body>
 <div id="container">
@@ -410,107 +406,190 @@ function f(n) {
 	}
 	?>
 	</div>
-	<div id="listingcontainer">
-		<div id="listingheader">
-	<div class="file"><a href="<?php echo $baseurl . $fileurl;?>">File</a></div>
-	<div class="size"><a href="<?php echo $baseurl . $sizeurl;?>">Size</a></div>
-	<div class="modified"><a href="<?php echo $baseurl . $dateurl;?>">Last Modified</a></div>
-	</div>
-		<div id="listing">
-	<?php
-	$class = 'b';
-	if($dirok) {
-	?>
-	<div><a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode($dotdotdir);?>" class="<?php echo $class;?>"><img src="<?php echo $includeurl; ?>dlf/dirup.png" alt="Folder" /><strong>..</strong> <em>&nbsp;</em>&nbsp;</a></div>
-	<?php
-		if($class=='b') $class='w';
-		else $class = 'b';
-	}
-	$arsize = sizeof($dirs);
-	for($i=0;$i<$arsize;$i++) {
-	?>
-	<div><a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode(str_replace($startdir,'',$leadon).$dirs[$i]);?>" class="<?php echo $class;?>"><img src="<?php echo $includeurl; ?>dlf/folder.png" alt="<?php echo $dirs[$i];?>" /><strong><?php echo $dirs[$i];?></strong> <em>-</em> <?php echo date ("M d Y h:i:s A", filemtime($includeurl.$leadon.$dirs[$i]));?></a></div>
-	<?php
-		if($class=='b') $class='w';
-		else $class = 'b';
-	}
+	<table id="listingcontainer">
 
-	$arsize = sizeof($files);
-	for($i=0;$i<$arsize;$i++) {
-		$icon = 'unknown.png';
-		$ext = strtolower(substr($files[$i], strrpos($files[$i], '.')+1));
-		$supportedimages = array('gif', 'png', 'jpeg', 'jpg');
-		$thumb = '';
+		<thead id="listingheader">
+			<tr>
+				<th class="file"><a href="<?php echo $baseurl . $fileurl;?>">File</a></th>
+				<th class="name"><a href="<?php echo $baseurl . $fileurl;?>">Name</a></th>
+				<th class="size"><a href="<?php echo $baseurl . $sizeurl;?>">Size</a></th>
+				<th class="modified"><a href="<?php echo $baseurl . $dateurl;?>">Last Modified</a></th>
+			</tr>
+		</thead>
 
-		if($showthumbnails && in_array($ext, $supportedimages)) {
-			$thumb = '<span><img src="dlf/trans.gif" alt="'.$files[$i].'" name="thumb'.$i.'" /></span>';
-			$thumb2 = ' onmouseover="o('.$i.', \''.urlencode($leadon . $files[$i]).'\');" onmouseout="f('.$i.');"';
 
-		}
+		<tbody id="listing">
 
-		if($filetypes[$ext]) {
-			$icon = $filetypes[$ext];
-		}
 
-		$filename = $files[$i];
-		if(strlen($filename)>43) {
-			$filename = substr($files[$i], 0, 40) . '...';
-		}
+			<?php $class = 'b'; ?>
+			<?php if($dirok) :  ?>
+				<tr class="<?php echo $class;?>">
+					<td>
+						<a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode($dotdotdir);?>">
+							<img src="<?php echo $includeurl; ?>dlf/dirup.png" alt="Folder" />
+						</a>
+					</td>
+					<td>
+						<a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode($dotdotdir);?>">
+							<strong>..</strong>
+						</a>
+					</td>
+					<td class="size">
+						<a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode($dotdotdir);?>">
+							<em></em>
+						</a>
+					</td>
+					<td class="modified">
+						<a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode(str_replace($startdir,'',$leadon).$dirs[$i]);?>">
+							<?php echo date ("M d Y h:i:s A", filemtime($includeurl.$leadon.$dirs[$i]));?>
+						</a>
+					</td>
+				</tr>
+				<?php
+					if($class=='b') $class='w';
+					else $class = 'b';
+				?>
+			<?php endif; ?>
 
-		$fileurl = $includeurl . $leadon . $files[$i];
-		if($forcedownloads) {
-			$fileurl = $_SESSION['PHP_SELF'] . '?dir=' . urlencode(str_replace($startdir,'',$leadon)) . '&download=' . urlencode($files[$i]);
-		}
 
-	?>
-	<div><a href="<?php echo $fileurl;?>" class="<?php echo $class;?>"<?php echo $thumb2;?>><img src="<?php echo $includeurl; ?>dlf/<?php echo $icon;?>" alt="<?php echo $files[$i];?>" /><strong><?php echo $filename;?></strong> <em><?php echo round(filesize($includeurl.$leadon.$files[$i])/1024);?>KB</em> <?php echo date ("M d Y h:i:s A", filemtime($includeurl.$leadon.$files[$i]));?><?php echo $thumb;?></a></div>
-	<?php
-		if($class=='b') $class='w';
-		else $class = 'b';
-	}
-	?></div>
-	<?php
-	if($allowuploads) {
-		$phpallowuploads = (bool) ini_get('file_uploads');
-		$phpmaxsize = ini_get('upload_max_filesize');
-		$phpmaxsize = trim($phpmaxsize);
-		$last = strtolower($phpmaxsize{strlen($phpmaxsize)-1});
-		switch($last) {
-			case 'g':
-				$phpmaxsize *= 1024;
-			case 'm':
-				$phpmaxsize *= 1024;
-		}
+			<?php $arsize = sizeof($dirs); ?>
+			<?php for($i=0;$i<$arsize;$i++) : ?>
+				<tr class="<?php echo $class;?>">
+					<td>
+						<a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode(str_replace($startdir,'',$leadon).$dirs[$i]);?>">
+							<img src="<?php echo $includeurl; ?>dlf/folder.png" alt="<?php echo $dirs[$i];?>" />
+						</a>
+					</td>
+					<td>
+						<a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode(str_replace($startdir,'',$leadon).$dirs[$i]);?>">
+							<strong><?php echo $dirs[$i];?></strong>
+						</a>
+					</td>
+					<td class="size">
+						<a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode(str_replace($startdir,'',$leadon).$dirs[$i]);?>">
+							<em>-</em>
+						</a>
+					</td>
+					<td class="modified">
+						<a href="<?php echo strip_tags($_SERVER['PHP_SELF']).'?dir='.urlencode(str_replace($startdir,'',$leadon).$dirs[$i]);?>">
+							<?php echo date ("M d Y h:i:s A", filemtime($includeurl.$leadon.$dirs[$i]));?>
+						</a>
+					</td>
+				</tr>
+				<?php
+					if($class=='b') $class='w';
+					else $class = 'b';
+				?>
+			<?php endfor; ?>
 
-	?>
-	<div id="upload">
-		<div id="uploadtitle">
-			<strong>File Upload</strong> (Max Filesize: <?php echo $phpmaxsize;?>KB)
 
-			<?php if($uploaderror) echo '<div class="upload-error">'.$uploaderror.'</div>'; ?>
-		</div>
-		<div id="uploadcontent">
+			<?php $arsize = sizeof($files); ?>
+			<?php for($i=0;$i<$arsize;$i++) : ?>
+				<?php
+					$icon = 'unknown.png';
+					$ext = strtolower(substr($files[$i], strrpos($files[$i], '.')+1));
+					$supportedimages = array('gif', 'png', 'jpeg', 'jpg');
+					$thumb = '';
+
+					if($showthumbnails && in_array($ext, $supportedimages)) {
+						$thumb = '<span><img src="dlf/trans.gif" alt="'.$files[$i].'" name="thumb'.$i.'" /></span>';
+						$thumb2 = ' onmouseover="o('.$i.', \''.urlencode($leadon . $files[$i]).'\');" onmouseout="f('.$i.');"';
+
+					}
+
+					if($filetypes[$ext]) {
+						$icon = $filetypes[$ext];
+					}
+
+					$filename = $files[$i];
+					if(strlen($filename)>43) {
+						$filename = substr($files[$i], 0, 40) . '...';
+					}
+
+					$fileurl = $includeurl . $leadon . $files[$i];
+					if($forcedownloads) {
+						$fileurl = $_SESSION['PHP_SELF'] . '?dir=' . urlencode(str_replace($startdir,'',$leadon)) . '&download=' . urlencode($files[$i]);
+					}
+				?>
+				<tr class="<?php echo $class;?>">
+					<td>
+						<a href="<?php echo $fileurl;?>"<?php echo $thumb2;?>>
+							<img src="<?php echo $includeurl; ?>dlf/<?php echo $icon;?>" alt="<?php echo $files[$i];?>" />
+						</a>
+					</td>
+					<td>
+						<a href="<?php echo $fileurl;?>"<?php echo $thumb2;?>>
+							<strong><?php echo $filename;?></strong>
+						</a>
+					</td>
+					<td class="size">
+						<a href="<?php echo $fileurl;?>"<?php echo $thumb2;?>>
+							<em><?php echo round(filesize($includeurl.$leadon.$files[$i])/1024);?>KB</em>
+						</a>
+					</td>
+					<td class="modified">
+						<a href="<?php echo $fileurl;?>"<?php echo $thumb2;?>>
+							<?php echo date ("M d Y h:i:s A", filemtime($includeurl.$leadon.$files[$i]));?><?php echo $thumb;?>
+						</a>
+					</td>
+				</tr>
+				<?php
+					if($class=='b') $class='w';
+					else $class = 'b';
+				?>
+			<?php endfor; ?>
+
+		</tbody>
+
+
+
+
+
+		<?php if($allowuploads) : ?>
 			<?php
-			if($phpallowuploads) {
-			?>
-			<form method="post" action="<?php echo strip_tags($_SERVER['PHP_SELF']);?>?dir=<?php echo urlencode(str_replace($startdir,'',$leadon));?>" enctype="multipart/form-data">
-			<input type="file" name="file" /> <input type="submit" value="Upload" />
-			</form>
-			<?php
-			}
-			else {
-			?>
-			File uploads are disabled in your php.ini file. Please enable them.
-			<?php
-			}
-			?>
-		</div>
 
-	</div>
-	<?php
-	}
-	?>
-	</div>
+					$phpallowuploads = (bool) ini_get('file_uploads');
+					$phpmaxsize = ini_get('upload_max_filesize');
+					$phpmaxsize = trim($phpmaxsize);
+					$last = strtolower($phpmaxsize{strlen($phpmaxsize)-1});
+					switch($last) {
+						case 'g':
+							$phpmaxsize *= 1024;
+						case 'm':
+							$phpmaxsize *= 1024;
+					}
+			?>
+			<div id="upload">
+				<div id="uploadtitle">
+					<strong>File Upload</strong> (Max Filesize: <?php echo $phpmaxsize;?>KB)
+
+					<?php if($uploaderror) echo '<div class="upload-error">'.$uploaderror.'</div>'; ?>
+				</div>
+				<div id="uploadcontent">
+					<?php
+					if($phpallowuploads) {
+					?>
+					<form method="post" action="<?php echo strip_tags($_SERVER['PHP_SELF']);?>?dir=<?php echo urlencode(str_replace($startdir,'',$leadon));?>" enctype="multipart/form-data">
+					<input type="file" name="file" /> <input type="submit" value="Upload" />
+					</form>
+					<?php
+					}
+					else {
+					?>
+					File uploads are disabled in your php.ini file. Please enable them.
+					<?php
+					}
+					?>
+				</div>
+
+			</div>
+		<?php endif; ?>
+
+
+
+
+
+	</table>
 </div>
 <div id="copy">Directory Listing Script &copy;2008 Evoluted, <a href="http://www.evoluted.net/">Web Design Sheffield</a>.</div>
 </body>
